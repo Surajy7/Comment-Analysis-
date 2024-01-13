c18eaee0f6932fc573ce32db7443adb74d32e628
Suraj Yadav <itssurajyadav7@gmail.com>
2024-01-13 20:31:22 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,7 +7,7 @@ import dash_html_components
 import dash_core_components as dcc  
 import dash_bootstrap_components as dbc 
 from dash_html_components import H1 
-import pandas as pd
+import pandas as pd 
 import dash
 import dash_html_components as html 
 import webbrowser
